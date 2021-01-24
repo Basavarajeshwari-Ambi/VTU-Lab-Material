48976856b9fdc743815408041ddd1f2a038d7693
Basavarajeshwari Ambi <42089257+Basavarajeshwari-Ambi@users.noreply.github.com>
2021-01-24 20:55:14 +0530

    Update index.php

--- sem-7/information_science/15CSL77_WEB_TECHNOLOGY_LABORATORY_WITH_MINI_PROJECT/program-07/index.php
+++ sem-7/information_science/15CSL77_WEB_TECHNOLOGY_LABORATORY_WITH_MINI_PROJECT/program-07/index.php
@@ -1,15 +1,12 @@
 <html>
-<head>
-<meta http-equiv="refresh" content="1">
-<title>Digital Clock</title>
-<style type="text/css">
-h1 {text-align: center}
-</style>
-</head>
-<body>
-<?php
-echo "<h1>Digital Clock</h1>"; echo "<hr/>";
-echo "<h1>".date('h:i:s A')."</h1>"; echo "<hr/>";
-?>
-</body>
+	<head>
+		<meta http-equiv="refresh" content="1">
+		<title>Digital Clock</title>
+	</head>
+	<body>
+		<?php
+			echo "<h1 align=center>Digital Clock</h1>"; echo "<hr/>";
+			echo "<h1 align=center>".date('h:i:s A')."</h1>"; echo "<hr/>";
+		?>
+	</body>
 </html>
